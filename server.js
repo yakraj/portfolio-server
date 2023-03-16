d700d9da1b94a7f1c9ceb8a1577ca56ce435e04a
yakraj <princeyakraj289@gmail.com>
2023-03-17 00:35:24 +0530

    updated

--- server.js
+++ server.js
@@ -32,15 +32,15 @@ cloudinary.config({
 
 // that is time to define server configuration
 
-const db = knex({
-  client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "yakraj",
-    database: "postgres",
-  },
-});
+// const db = knex({
+//   client: "pg",
+//   connection: {
+//     host: "127.0.0.1",
+//     user: "postgres",
+//     password: "yakraj",
+//     database: "postgres",
+//   },
+// });
 // create table mega_projects(id serial primary key,title text,description text,url text,date text,images text [],tags text, projectid text,post_type varchar(10))
 // const db = knex({
 //   client: "pg",
@@ -80,57 +80,57 @@ app.get("/", (req, res) => {
   //   });
 });
 
-app.post("/create/smallproject", (req, res) => {
-  var { title, desc, url, thumbnail, format, post_type } = req.body;
-  var adid = uniqid(title, new Date());
-  db("small_projects")
-    .insert({
-      title: title,
-      description: desc,
-      url: url,
-      date: new Date(),
-      thumbnail: thumbnail,
-      projectid: adid,
-      thumb_format: format,
-      post_type: post_type,
-    })
-    .then((response) => {
-      res.json("successfully created");
-    })
-    .catch((err) => res.status(400).json("unable to create data"));
-});
+// app.post("/create/smallproject", (req, res) => {
+//   var { title, desc, url, thumbnail, format, post_type } = req.body;
+//   var adid = uniqid(title, new Date());
+//   db("small_projects")
+//     .insert({
+//       title: title,
+//       description: desc,
+//       url: url,
+//       date: new Date(),
+//       thumbnail: thumbnail,
+//       projectid: adid,
+//       thumb_format: format,
+//       post_type: post_type,
+//     })
+//     .then((response) => {
+//       res.json("successfully created");
+//     })
+//     .catch((err) => res.status(400).json("unable to create data"));
+// });
 
-app.get("/smallproject", (req, res) => {
-  console.log(req);
-  db("small_projects")
-    .select("*")
-    .then((response) => res.json(response));
-});
-app.get("/megaproject", (req, res) => {
-  db("mega_projects")
-    .select("*")
-    .then((response) => res.json(response));
-});
-app.post("/create/megaproject", (req, res) => {
-  var { title, desc, url, format, tags, images, post_type } = req.body;
+// app.get("/smallproject", (req, res) => {
+//   console.log(req);
+//   db("small_projects")
+//     .select("*")
+//     .then((response) => res.json(response));
+// });
+// app.get("/megaproject", (req, res) => {
+//   db("mega_projects")
+//     .select("*")
+//     .then((response) => res.json(response));
+// });
+// app.post("/create/megaproject", (req, res) => {
+//   var { title, desc, url, format, tags, images, post_type } = req.body;
 
-  var adid = uniqid(title);
-  console.log(adid);
-  db("mega_projects")
-    .insert({
-      title: title,
-      description: desc,
-      url: url,
-      date: Date(),
-      images: images,
-      projectid: adid,
-      tags: tags,
-      post_type: post_type,
-    })
-    .then((response) => {
-      res.json("successfully created");
-    });
-});
+//   var adid = uniqid(title);
+//   console.log(adid);
+//   db("mega_projects")
+//     .insert({
+//       title: title,
+//       description: desc,
+//       url: url,
+//       date: Date(),
+//       images: images,
+//       projectid: adid,
+//       tags: tags,
+//       post_type: post_type,
+//     })
+//     .then((response) => {
+//       res.json("successfully created");
+//     });
+// });
 
 app.listen(process.env.PORT || 5001, () => {
   console.log(`app is running on port ${process.env.PORT}`);
